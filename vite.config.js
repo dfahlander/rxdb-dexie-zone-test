a02cb2861b682dc0f9d3ee70357dfc41729bca63
David Fahlander <david@dexie.org>
2024-09-02 10:46:36 +0200

    Transpile await async/await.
    What I did:
    
    npm i -D vite-plugin-babel
    npm i -D @babel/plugin-transform-async-to-generator
    
    ...and then added the vite-plugin-babel to vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import babel from 'vite-plugin-babel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    babel({
+      babelConfig: {
+        babelrc: false,
+        configFile: false,
+        plugins: ["@babel/plugin-transform-async-to-generator"],
+      },
+    })
+  ],
   // optimizeDeps: {
   //   exclude: ['@socotec.io/socio-vue-components']
   // }
-})
+});
